302d0b87056d1a0fb215794fec01d0b8fe227dc7
François Caremoli <francois.caremoli@wanadoo.fr>
2024-05-07 14:35:03 +0200

    Ajout correcions exercices conversions et instanciations

---
+++ src/main/java/com/bigcorp/formation/correction/exercice/ExerciceConversions.java
@@ -0,0 +1,21 @@
+/**
+ * Exemple sur les variables
+ */
+
+package com.bigcorp.formation.correction.exercice;
+
+// Classe principale
+public class ExerciceConversions {
+    public static void main(String[] args) {
+        // Conversion d'un short en int
+    	short court = 3;
+    	int moinsCourt = court;
+    	
+    	
+    	//Conversion d'un int en short
+    	int superInt = 20000;
+    	short pasSuperShort = (short)superInt;
+    	
+     
+    }
+}
---
+++ src/main/java/com/bigcorp/formation/correction/exercice/ExerciceInstanciations.java
@@ -0,0 +1,22 @@
+/**
+ * Exemple sur les variables
+ */
+
+package com.bigcorp.formation.correction.exercice;
+
+// Classe principale
+public class ExerciceInstanciations {
+	public static void main(String[] args) {
+		// Définir une variable de type Object
+		Object monObjet;
+		
+		//Définition et instanciation d'un nouvel objet dans une nouvelle variable
+		Object monObjet2 = new Object();
+		
+		//Affectation de la valeur null à cet objet
+		monObjet2 = null;
+		
+		
+
+	}
+}
