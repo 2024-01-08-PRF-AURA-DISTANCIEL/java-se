0c6d7a28892fcf27a98cf795a1b25b232666d0f2
François Caremoli <francois.caremoli@wanadoo.fr>
2024-05-16 16:49:02 +0200

    Ajout du cours sur les exceptions

--- src/main/java/com/bigcorp/formation/cours/Exceptions.java
+++ src/main/java/com/bigcorp/formation/cours/Exceptions.java
@@ -1,23 +1,51 @@
 package com.bigcorp.formation.cours;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Exceptions {
 
 	public static void main(String[] args) {
-		//Appel de methode qui ne marche pas top
-		//et utilisation des méthodes de l'exception
-		
-		//try catch de lecture de smon fichier
-//		File monFichier = new File("mon-fichier.txt");
-//		FileInputStream fileInputStream = null;
-//			fileInputStream = new FileInputStream(monFichier);
-//			byte[] fileBytes = fileInputStream.readAllBytes();
-		
-		//try with resources
+		// Appel de methode qui ne marche pas top
+		// et utilisation des méthodes de l'exception
+		try {
+			System.out.println("Je suis au début du try");
+			methodeQuiMarchePasTop();
+			System.out.println("Je suis à la fin du try");
+		} catch (MonException e) {
+			System.out.println("Je suis au début du catch");
+			System.out.println(e.getMessage());
+			System.out.println("La gravité vaut : " + e.isGrave());
+		} finally {
+			System.out.println("Ici, une ligne qui s'affiche tout le temps");
+		}
+		System.out.println("Je suis sorti du try/catch");
+
+		// try with resources lisant un fichier.
+		//Ici, on crée un fileInputStream , qui est automatiquement
+		//fermé qu'une exception se produise, ou pas.
+		File monFichier = new File("mon-fichier.txt");
+		try(FileInputStream fileInputStream = new FileInputStream(monFichier)){
+			//Copie d'un fichier dans un tableau d'octets
+			byte[] fileBytes = fileInputStream.readAllBytes();
+		} catch (IOException e) {
+			System.out.println("Une erreur d'entrée/sortie s'est produite : " + e.getMessage());
+		}
+
 
 	}
-	
-	public static void methodeQuiMarchePasTop() throws MonException{
-		throw new MonException("Bon ben là, ça marche pas !" , true);
+
+	/**
+	 * Méthode qui lance systématiquement une instance de MonException
+	 * 
+	 * @throws MonException
+	 */
+	public static void methodeQuiMarchePasTop() throws MonException {
+
+		System.out.println("Je bosse");
+		// calcul tres complique
+		throw new MonException("Bon ben là, ça marche pas ", true);
 	}
 
 }
